514060930c8686479742f7d8a89e273858a8626b
lequyet2001 <lequyet840500@gmail.com>
2021-04-24 10:01:23 +0700

    update

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(morgan("dev"));
 app.use(cors())
 
 
-app.use(expressValidator())
+// app.use(expressValidator())
 
 
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { signup, signin,signout} from "../controller/authController";
-import {userSignupValidator} from '../validator'
 
-router.post('/signup', userSignupValidator ,signup);
+
+router.post('/signup' ,signup);
 router.post('/signin', signin);
 router.get('/signin', signin);
 router.get('/signout', signout)
