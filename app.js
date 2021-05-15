76f914760d91b62f1def7f2d1845c09c477e0a04
lequyet2001 <lequyet840500@gmail.com>
2021-05-15 14:52:26 +0700

    update home

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 //start tạo ra hàm connect
 const connect = () =>
   mongoose
-    .connect(process.env.MONGODB || "mongodb://localhost:27017/quyet_buy",
+    .connect(process.env.MONGODB_URL || "mongodb://localhost:27017/quyet_buy",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
