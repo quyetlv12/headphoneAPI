439b16801887fb07920c790808d507049ba05583
lequyet2001 <lequyet840500@gmail.com>
2021-06-01 14:35:45 +0700

    update

--- model/categoryModel.js
+++ model/categoryModel.js
@@ -1,12 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
+const Categories = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-const Categories = new Schema({
-    name : {
-        type : String,
-        require : true
-    }
-},{timestamps : true})
-
-module.exports = mongoose.model("Categories",Categories);
+module.exports = mongoose.model("Categories", Categories);
