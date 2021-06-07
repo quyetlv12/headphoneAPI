e488744cb0a22e5d41a705eae7b5628ebbd24a04
lequyet2001 <lequyet840500@gmail.com>
2021-06-07 11:18:53 +0700

    update new

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import CategoryRouter from "./routes/cateRouter";
 import NewsRouter from "./routes/newsRouter";
 import productRouter from "./routes/productsRouter";
 import userRouter from "./routes/userRouter";
-import ContactRouter from './routes/ContactRouter'
+import ContactRouter from "./routes/ContactRouter";
 const mongoose = require("mongoose");
 
 //start yêu cầu hình env trong file app
@@ -59,7 +59,17 @@ app.use("/api", NewsRouter);
 app.use("/api", authRouter);
 app.use("/api", ContactRouter);
 app.get("/", (req, res) => {
-  return res.send(/*html*/ "<h1>CHÀO MỪNG ĐẾN VỚI HEADPHONE API</h1>");
+  return res.send(
+    /*html*/ `<div style="display:flex;justify-content:center">
+    <img src="https://i.pinimg.com/originals/b9/7d/c2/b97dc288d71e7938c1ce8b7faacdc9ac.gif" alt=""></div>
+    <ul style="list-style:none;text-decoration:none">
+    <li><a style="text-decoration:none" href="https://headphoneapi.herokuapp.com/api/product">Product</a></li>
+    <li><a style="text-decoration:none" href="https://headphoneapi.herokuapp.com/api/categories">Category</a></li>
+    <li><a style="text-decoration:none" href="https://headphoneapi.herokuapp.com/api/news">News</a></li>
+    <li><a style="text-decoration:none" href="https://headphoneapi.herokuapp.com/api/contact">Contact</a></li>
+    </ul>
+    `
+  );
 });
 // start lắng nghe cổng 4000
 app.listen(port, () => {
