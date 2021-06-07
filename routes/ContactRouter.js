afb832c8c90ae3ed75e10c5ebf3fb802b20336a3
lequyet2001 <lequyet840500@gmail.com>
2021-06-07 10:31:02 +0700

    update new

--- routes/ContactRouter.js
+++ routes/ContactRouter.js
@@ -6,7 +6,7 @@ import { userById } from '../controller/userController';
 const ContactRouter = express.Router();
 ContactRouter.get('/contact' , showListContact);
 ContactRouter.get('/contact/:contactId' , ContactDetail);
-ContactRouter.post('/contact/:UserId',requireSignin,isAuth,isAdmin,addContact)
+ContactRouter.post('/contact',addContact)
 ContactRouter.put('/contact/:contactId/:UserId',requireSignin,isAuth,isAdmin,editContact)
 ContactRouter.delete('/contact/:contactId/:UserId',requireSignin,isAuth,isAdmin,deleteContact)
 ContactRouter.param('contactId' , newById)
