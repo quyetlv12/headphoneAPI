af1fb71bf5656476be8cd48f473094ace097b154
lequyet2001 <lequyet840500@gmail.com>
2021-06-07 09:18:16 +0700

    update new

--- controller/newsController.js
+++ controller/newsController.js
@@ -1,70 +1,84 @@
-
-
 //start import model News
-import News from '../model/newsModel';
-
-
-
-//start add News 
-
-export const addNews = (req,res,next) =>{
-  res.json({
-    name : "that is add News"
-  })
-}
+import News from "../model/newsModel";
+import _ from "lodash";
+//start add News
+export const newById = (req, res, next, id) => {
+  News.findById(id).exec((err, news) => {
+    if (err || !news) {
+      res.status(400).json({
+        error: "error",
+      });
+    }
+    req.news = news;
+    next();
+  });
+};
+export const addNews = (req, res, next) => {
+  const news = new News(req.body);
+  news.save((err, news) => {
+    if (err) {
+      res.status(400).json({
+        err: "thêm bài viết không thành công",
+      });
+    }
+    res.status(200).json(news);
+  });
+};
 
 //end add News
 
 //start show detail Newsr
 
+//start delete News
 
-
-
-//start delete News 
-
-export const deleteNews = (req,res,next) =>{
-  res.json({
-    name : "that is delete News"
-  })
-}
+export const deleteNews = (req, res, next) => {
+  let news = req.news;
+  news.remove((err, db) => {
+    if (err) {
+      res.json({
+        error: "xoá không thành công",
+      });
+    }
+    res.json({
+      db,
+      message: `xoá thành công sản phẩm ${db.name}`,
+    });
+  });
+};
 
 //end delete News
 
 //start edit News
-export const editNews = (req,res,next) =>{
-  res.json({
-    name : "that is edit News"
-  })
-}
+export const editNews = (req, res, next) => {
+  let news = _.assignIn(req.news, req.body);
+  news.save((err, db) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(400).json({
+        error: "Cập nhật sản phẩm không thành công",
+      });
+    } else {
+      res.json({
+        message: "Sửa sản phẩm thành công",
+      });
+    }
+  });
+};
 //end edit News
 
-
-
 //start show list News
-export const showListNews = (req,res,next) =>{
-    News.find({})
-    .then(News => {
-      News = News.map(News => News.toObject())
-      res.json(News)
-    }
-    )
-    .catch(next)
-        
-}
+export const showListNews = (req, res, next) => {
+  News.find({})
+    .then((News) => {
+      News = News.map((News) => News.toObject());
+      res.json(News);
+    })
+    .catch(next);
+};
 
 //end show list News
 
-
-//start show detail 
+//start show detail
 export const newsDetail = (req, res, next) => {
-  const id = req.params.id;
-  News.findById(id, (err, db) => {
-    if (err) {
-      res.json({
-        message: "sản phẩm không tồn tại",
-      });
-    } else {
-      res.json(db == null ? "sản phẩm không tồn tại" : db);
-    }
-  });
+  return res.json(req.news);
 };
--- routes/newsRouter.js
+++ routes/newsRouter.js
@@ -1,6 +1,15 @@
-import { showListNews,newsDetail} from '../controller/newsController';
+import { showListNews,newsDetail,addNews,deleteNews,editNews,newById} from '../controller/newsController';
+import {requireSignin, isAuth,isAdmin} from '../controller/authController'
+
 import express from 'express'
+import { userById } from '../controller/userController';
 const NewsRouter = express.Router();
 NewsRouter.get('/news' , showListNews);
-NewsRouter.get('/news/:id' , newsDetail);
+NewsRouter.get('/news/:newsId' , newsDetail);
+NewsRouter.post('/news/:UserId',requireSignin,isAuth,isAdmin,addNews)
+NewsRouter.put('/news/:newsId/:UserId',requireSignin,isAuth,isAdmin,editNews)
+NewsRouter.delete('/news/:newsId/:UserId',requireSignin,isAuth,isAdmin,deleteNews)
+NewsRouter.param('newsId' , newById)
+NewsRouter.param('UserId', userById)
+
 module.exports = NewsRouter;
